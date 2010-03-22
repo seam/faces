2d487e19ea535dd294fe9f4c57fff59381621baf
Nicklas Karlsson <nickarls@gmail.com>
2010-03-22 19:50:14 +0000

    look for BeanManager in some more exotic (JBoss) locations

--- src/main/java/org/jboss/seam/faces/event/GenericEventListener.java
+++ src/main/java/org/jboss/seam/faces/event/GenericEventListener.java
@@ -1,14 +1,28 @@
 package org.jboss.seam.faces.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+@SuppressWarnings("serial")
 public class GenericEventListener
 {
+
    @Inject
    BeanManager beanManager;
+
+   // FIXME: hack to work around invalid binding in JBoss AS 6 M2
+   private static final List<String> beanManagerLocations = new ArrayList<String>()
+   {
+      {
+         add("java:comp/BeanManager");
+         add("java:app/BeanManager");
+      }
+   };
 
    protected BeanManager getBeanManager()
    {
@@ -21,13 +35,17 @@ public class GenericEventListener
 
    private BeanManager lookupBeanManager()
    {
-      try
+      for (String location : beanManagerLocations)
       {
-         return beanManager = (BeanManager) new InitialContext().lookup("java:comp/BeanManager");
+         try
+         {
+            return (BeanManager) new InitialContext().lookup(location);
+         }
+         catch (NamingException e)
+         {
+            // No panic, keep trying
+         }
       }
-      catch (NamingException e)
-      {
-         throw new IllegalArgumentException("Could not find BeanManager in JNDI", e);
-      }
+      throw new IllegalArgumentException("Could not find BeanManager in " + beanManagerLocations);
    }
 }
