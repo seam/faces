8dc2c631e7d52e2d6ee1185d743e47d3cbff569f
Nicklas Karlsson <nickarls@gmail.com>
2010-04-08 09:43:36 +0000

    Update license headers

--- impl/src/main/java/org/jboss/seam/faces/event/SystemEventBridge.java
+++ impl/src/main/java/org/jboss/seam/faces/event/SystemEventBridge.java
@@ -1,6 +1,6 @@
 /*
- * JBoss, Community-driven Open Source Middleware
- * Copyright 2010, JBoss by Red Hat, Inc., and individual contributors
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
