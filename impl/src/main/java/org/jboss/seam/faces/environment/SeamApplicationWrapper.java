36952ca9f823b02463a8f9419fb82cf2a161a17b
Brian Leathem <bleathem@gmail.com>
2011-02-11 08:36:58 -0800

    Added debug messages to the extension of Converters and Validators

--- impl/src/main/java/org/jboss/seam/faces/environment/SeamApplicationWrapper.java
+++ impl/src/main/java/org/jboss/seam/faces/environment/SeamApplicationWrapper.java
@@ -32,6 +32,7 @@ import javax.faces.convert.Converter;
 import javax.faces.event.PostConstructApplicationEvent;
 import javax.faces.validator.Validator;
 import javax.inject.Inject;
+import org.jboss.logging.Logger;
 
 import org.jboss.seam.faces.util.BeanManagerUtils;
 
@@ -45,6 +46,8 @@ import org.jboss.seam.faces.util.BeanManagerUtils;
 @ApplicationScoped
 public class SeamApplicationWrapper extends ApplicationWrapper
 {
+   private transient final Logger log = Logger.getLogger(SeamApplicationWrapper.class);
+    
    private Application parent;
 
    @Inject
@@ -58,6 +61,7 @@ public class SeamApplicationWrapper extends ApplicationWrapper
 
    public void installWrapper(@Observes final PostConstructApplicationEvent event)
    {
+      log.debug("PostConstructApplicationEvent observed, installing wrapper");
       ApplicationFactory factory = (ApplicationFactory) FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
       parent = event.getApplication();
       factory.setApplication(this);
@@ -66,6 +70,7 @@ public class SeamApplicationWrapper extends ApplicationWrapper
    @Override
    public Converter createConverter(final Class<?> targetClass)
    {
+      log.debugf("Creating converter for targetClass %s", targetClass.getName());
       Converter result = parent.createConverter(targetClass);
       result = attemptExtension(result);
       return result;
@@ -74,6 +79,7 @@ public class SeamApplicationWrapper extends ApplicationWrapper
    @Override
    public Converter createConverter(final String converterId)
    {
+      log.debugf("Creating converter for converterId %s", converterId);
       Converter result = parent.createConverter(converterId);
       result = attemptExtension(result);
       return result;
@@ -82,6 +88,7 @@ public class SeamApplicationWrapper extends ApplicationWrapper
    @Override
    public Validator createValidator(final String validatorId)
    {
+      log.debugf("Creating validator for validatorId %s", validatorId);
       Validator result = parent.createValidator(validatorId);
       result = attemptExtension(result);
       return result;
@@ -92,20 +99,25 @@ public class SeamApplicationWrapper extends ApplicationWrapper
    {
       T result = base;
 
-      if (base != null)
+      if (base == null)
       {
-         if (managerUtils.isDependentScoped(base.getClass()))
-         {
-            managerUtils.injectNonContextualInstance(result);
-         }
-         else
-         {
-            result = (T) managerUtils.getContextualInstance(base.getClass());
-         }
+         log.warnf("Cannot attempt extension on null");
+         return null;
+      }
+
+      log.debugf("Extending class: %s", base.getClass().getName());
+      if (managerUtils.isDependentScoped(base.getClass()))
+      {
+         managerUtils.injectNonContextualInstance(result);
+      }
+      else
+      {
+         result = (T) managerUtils.getContextualInstance(base.getClass());
       }
 
       if (result == null)
       {
+         log.warnf("Using JSF provided instance, unable to find a BeanManaged instance for class %s", base.getClass().getName());
          result = base;
       }
 
