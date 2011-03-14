81c622eaacdfa720db2f1d2894f55b2f9f92576b
joserodolfofreitas <joserodolfo.freitas@gawab.com>
2011-03-14 19:28:06 -0300

    add some comments on InputElement class

--- api/src/main/java/org/jboss/seam/faces/validation/InputElement.java
+++ api/src/main/java/org/jboss/seam/faces/validation/InputElement.java
@@ -7,10 +7,23 @@ import javax.faces.component.UIInput;
 import org.jboss.seam.solder.core.Veto;
 
 /**
+ * To be used in conjunction with <code>&lt;s:validateForm /&gt;</code> in
+ * Validators that should have their values fetched from a JSF form field.
+ * 
+ * Example of injection:
+ * 
+ * @Inject
+ * private InputElement<String> firstName;
+ *
+ * @Inject
+ * private InputElement<String> lastName;
+ * 
+ * @Inject
+ * private InputElement<Date> dateOfBirth;
  * 
  * @author <a href="http://community.jboss.org/people/spinner)">Jose Rodolfo freitas</a>
  *
- * @param <T>
+ * @param <T> is the type of value the inputElement holds.
  */
 @Veto
 public class InputElement<T> implements Serializable{
