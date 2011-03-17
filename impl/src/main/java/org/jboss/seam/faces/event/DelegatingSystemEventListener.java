9d1210bb3c0eeb28177b1b3462083cc734c70eb6
Dan Allen <dan.j.allen@gmail.com>
2011-03-17 02:53:46 -0400

    SEAMFACES-96 don't attempt to propogate PreDestoryApplicationEvent

--- impl/src/main/java/org/jboss/seam/faces/event/DelegatingSystemEventListener.java
+++ impl/src/main/java/org/jboss/seam/faces/event/DelegatingSystemEventListener.java
@@ -3,8 +3,11 @@ package org.jboss.seam.faces.event;
 import java.util.List;
 
 import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PreDestroyApplicationEvent;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
+
+import org.jboss.logging.Logger;
 
 /**
  * Provide CDI injection to SystemEventListener artifacts by delegating through
@@ -15,7 +18,8 @@ import javax.faces.event.SystemEventListener;
  */
 public class DelegatingSystemEventListener extends AbstractListener<SystemEventListener> implements SystemEventListener
 {
-
+   private static final Logger log = Logger.getLogger(DelegatingSystemEventListener.class);
+   
    public boolean isListenerForSource(final Object source)
    {
       return true;
@@ -23,6 +27,12 @@ public class DelegatingSystemEventListener extends AbstractListener<SystemEventL
 
    public void processEvent(final SystemEvent event) throws AbortProcessingException
    {
+      if (event instanceof PreDestroyApplicationEvent && !isBeanManagerAvailable())
+      {
+         log.info("BeanManager no longer available; Cannot notify CDI-managed listeners of " + PreDestroyApplicationEvent.class.getSimpleName());
+         return;
+      }
+      
       for (SystemEventListener l : getEventListeners())
       {
          if (l.isListenerForSource(event.getSource()))
