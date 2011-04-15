c9c2a10c2b78ce7c7c4fe6d845a47d8cdd592653
Brian Leathem <bleathem@gmail.com>
2011-04-15 16:43:06 -0700

    Tidied up the visual dsiplay, and java code for the faces-viewconfig example app

--- examples/faces-viewconfig/src/main/java/org/jboss/seam/faces/examples/viewconfig/security/SecurityRules.java
+++ examples/faces-viewconfig/src/main/java/org/jboss/seam/faces/examples/viewconfig/security/SecurityRules.java
@@ -5,40 +5,26 @@ import org.jboss.seam.faces.examples.viewconfig.model.Current;
 import org.jboss.seam.faces.examples.viewconfig.model.Item;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.annotations.Secures;
+import org.picketlink.idm.api.User;
 
 /**
  *
  * @author <a href="mailto:bleathem@gmail.com">Brian Leathem</a>
  */
 public class SecurityRules {
-
-    private transient final Logger logger = Logger.getLogger(SecurityRules.class.getName());
-
     public @Secures @Owner boolean ownerChecker(Identity identity, @Current Item item) {
-        if (item == null) {
-            logger.info("Item is null");
+        if (item == null || identity.getUser() == null) {
             return false;
         } else {
-            return doesUsernameMatch(identity, item.getOwner());
+            return item.getOwner().equals(identity.getUser().getId());
         }
     }
 
-    public @Secures @Public boolean publicChecker() {
-        return true;
-    }
-    
     public @Secures @Admin boolean adminChecker(Identity identity) {
-        return doesUsernameMatch(identity, "admin");
-    }
-    
-    private boolean doesUsernameMatch(Identity identity, String expectedUserName) {
-        if (identity == null || identity.getUser() == null) {
-            logger.info("Identity/User is null");
+        if (identity.getUser() == null) {
             return false;
         } else {
-            String username = identity.getUser().getId();
-            logger.infof("Username is: %s", username);
-            return username.equals(expectedUserName);
+            return "admin".equals(identity.getUser().getId());
         }
     }
 }
