35c76c7026cbd1758eeb0eb23eb47272bd9ba7e5
LightGuard <lightguard.jp@gmail.com>
2011-05-10 02:27:56 -0600

    Fixing the build, sorry

--- impl/src/test/java/org/jboss/seam/faces/rewrite/RewriteConfigurationTest.java
+++ impl/src/test/java/org/jboss/seam/faces/rewrite/RewriteConfigurationTest.java
@@ -8,6 +8,8 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author bleathem
  */
--- impl/src/test/java/org/jboss/seam/faces/test/status/MessagesAdapterTest.java
+++ impl/src/test/java/org/jboss/seam/faces/test/status/MessagesAdapterTest.java
@@ -29,6 +29,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
