b26456ed09481611a7d52993d36ad838f6d36b1d
Brian Leathem <bleathem@gmail.com>
2011-06-15 23:30:21 -0700

    SEAMFACES-172: Follow Seam 3 example naming scheme

---
+++ examples/short-ly/src/main/java/org/jboss/seam/faces/examples/shortly/LinkBean.java
@@ -0,0 +1,77 @@
+package org.jboss.seam.faces.examples.shortly;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.jboss.seam.faces.context.conversation.Begin;
+import org.jboss.seam.faces.context.conversation.End;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@Named
+@ConversationScoped
+@Stateful
+public class LinkBean implements Serializable {
+    private static final long serialVersionUID = -2209547152337410725L;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private TinyLink link = new TinyLink();
+
+    @Begin
+    @End
+    public String createLink() throws SQLException {
+        System.out.println("Created link: [ " + link.getName() + " => " + link.getTarget() + " ]");
+        em.persist(link);
+        return "pretty:create";
+    }
+
+    @SuppressWarnings("unchecked")
+    public TinyLink getByKey(final String key) {
+        Query query = em.createQuery("from TinyLink t where t.name=:key", TinyLink.class);
+        query.setParameter("key", key);
+        List<TinyLink> resultList = query.getResultList();
+        if (resultList.isEmpty()) {
+            return new TinyLink();
+        }
+        return resultList.get(0);
+    }
+
+    public String format(final String link) {
+        if (link != null) {
+            String result = link.trim();
+            if (!result.matches("(http|ftp)://.*")) {
+                result = "http://" + result;
+            }
+            return result;
+        }
+        return "";
+    }
+
+    public String deleteAll() {
+        em.createQuery("delete from TinyLink").executeUpdate();
+        return "pretty:";
+    }
+
+    public List<TinyLink> getLinks() {
+        return em.createQuery("from TinyLink").getResultList();
+    }
+
+    public TinyLink getLink() {
+        return link;
+    }
+
+    public void setLink(final TinyLink link) {
+        this.link = link;
+    }
+}
---
+++ examples/short-ly/src/main/java/org/jboss/seam/faces/examples/shortly/RedirectBean.java
@@ -0,0 +1,40 @@
+package org.jboss.seam.faces.examples.shortly;
+
+import java.io.IOException;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@Named
+@RequestScoped
+public class RedirectBean {
+    @Inject
+    FacesContext context;
+
+    @Inject
+    LinkBean linkBean;
+
+    private String name;
+
+    public void send() throws IOException {
+        String url = linkBean.getByKey(name).getTarget();
+        System.out.println("Sent redirect for key: " + name + " => " + url);
+        HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+        response.sendRedirect(linkBean.format(url));
+        context.responseComplete();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String key) {
+        this.name = key;
+    }
+}
