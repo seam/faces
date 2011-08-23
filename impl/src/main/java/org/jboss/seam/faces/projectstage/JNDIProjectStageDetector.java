6e7d8189a6de0f2a7d5cbc698454eedf49ec0f83
Christian Kaltepoth <christian@kaltepoth.de>
2011-08-23 17:28:25 +0200

    First version of ProjectStageExtension. Currently only supports project stage configuration via JNDI

---
+++ impl/src/main/java/org/jboss/seam/faces/projectstage/JNDIProjectStageDetector.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.seam.faces.projectstage;
+
+import javax.faces.application.ProjectStage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * Implementation of {@link ProjectStageDetector} that performs a JNDI lookup to get the current project stage.
+ * 
+ * @author Christian Kaltepoth <christian@kaltepoth.de>
+ * 
+ */
+public class JNDIProjectStageDetector implements ProjectStageDetector {
+
+    @Override
+    public ProjectStage getProjectStage() {
+
+        // try to get the name of the project stage from JNDI
+        String projectStageName = getProjectStageNameFromJNDI();
+
+        // lookup the correct enum for the value found via JNDI
+        if (projectStageName != null && projectStageName.length() > 0) {
+            for (ProjectStage stage : ProjectStage.values()) {
+                if (stage.name().equalsIgnoreCase(projectStageName)) {
+                    return stage;
+                }
+            }
+        }
+
+        // no result
+        return null;
+
+    }
+
+    /**
+     * Performs a JNDI lookup to obtain the current project stage. The method use the standard JNDI name for the JSF project
+     * stage for the lookup
+     * 
+     * @return name bound to JNDI or <code>null</code>
+     */
+    private String getProjectStageNameFromJNDI() {
+
+        try {
+
+            InitialContext context = new InitialContext();
+            Object obj = context.lookup(ProjectStage.PROJECT_STAGE_JNDI_NAME);
+            if (obj != null) {
+                return obj.toString().trim();
+            }
+
+        } catch (NamingException e) {
+            // ignore
+        }
+
+        return null;
+    }
+
+}
---
+++ impl/src/main/java/org/jboss/seam/faces/projectstage/ProjectStageExtension.java
@@ -0,0 +1,145 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.seam.faces.projectstage;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.faces.application.ProjectStage;
+
+import org.jboss.seam.solder.logging.Logger;
+import org.jboss.seam.solder.util.service.ServiceLoader;
+
+/**
+ * Extension that supports to veto beans that should only be active in certain 
+ * JSF project stages.
+ * 
+ * @author Christian Kaltepoth <christian@kaltepoth.de>
+ * 
+ */
+public class ProjectStageExtension implements Extension {
+
+    private transient final Logger log = Logger.getLogger(ProjectStageExtension.class);
+
+    /**
+     * The default project stage
+     */
+    private final static ProjectStage DEFAULT_PROJECT_STAGE = ProjectStage.Production;
+
+    /**
+     * Field to store the detected project stage. The field is lazily initialized in
+     * {@link #processAnnotatedType(ProcessAnnotatedType)}
+     */
+    private ProjectStage stage;
+
+    /**
+     * Observes {@link ProcessAnnotatedType} events and sends a veto if the type should be 
+     * ignored due to the current project stage.
+     */
+    public <T> void processAnnotatedType(@Observes ProcessAnnotatedType<T> pat) {
+
+        // lazily obtain the project stage
+        if (stage == null) {
+            stage = detectProjectStage();
+        }
+
+        // the project stages explicitly specified on the type
+        Set<ProjectStage> validProjectStages = getProjectStagesForType(pat.getAnnotatedType());
+
+        // veto the type if the current project stage doesn't match
+        if (validProjectStages.size() > 0 && !validProjectStages.contains(stage)) {
+
+            if (log.isDebugEnabled()) {
+                log.debug("Preventing class " + pat.getAnnotatedType().getJavaClass().getName()
+                        + " from being installed due to the current project stage");
+            }
+
+            // veto this type
+            pat.veto();
+
+        }
+
+    }
+
+    /**
+     * Returns a set of {@link ProjectStage}s for which the supplied {@link AnnotatedType} has been enabled using one or more of
+     * the annotations {@link Development}, {@link Production}, {@link SystemTest} and {@link UnitTest}. The method will return
+     * an empty set if the type isn't annotated with any of the annotations.
+     * 
+     * @param type The {@link AnnotatedType} to process
+     * @return The list of project stages
+     */
+    private <T> Set<ProjectStage> getProjectStagesForType(AnnotatedType<T> type) {
+
+        Set<ProjectStage> stages = new HashSet<ProjectStage>();
+
+        if (type.getAnnotation(Development.class) != null) {
+            stages.add(ProjectStage.Development);
+        }
+        if (type.getAnnotation(Production.class) != null) {
+            stages.add(ProjectStage.Production);
+        }
+        if (type.getAnnotation(SystemTest.class) != null) {
+            stages.add(ProjectStage.SystemTest);
+        }
+        if (type.getAnnotation(UnitTest.class) != null) {
+            stages.add(ProjectStage.UnitTest);
+        }
+
+        return stages;
+
+    }
+
+    /**
+     * Method to detect the current project stage. The method will use the {@link ProjectStageDetector} SPI to obtain the
+     * project stage. The fall back value is {@link ProjectStage#Development}.
+     */
+    private ProjectStage detectProjectStage() {
+
+        // Get SPI implementations
+        Iterator<ProjectStageDetector> iterator = ServiceLoader.load(ProjectStageDetector.class).iterator();
+        while (iterator.hasNext()) {
+            ProjectStageDetector detector = iterator.next();
+
+            // lets the detector do its job
+            ProjectStage stage = detector.getProjectStage();
+
+            if (log.isTraceEnabled()) {
+                log.trace("Detector " + detector.getClass().getName() + " returned: " + stage);
+            }
+
+            if (stage != null) {
+                log.info("Detected project stage: " + stage);
+                return stage;
+            }
+
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("No result from detectors! Using default project stage: " + DEFAULT_PROJECT_STAGE);
+        }
+
+        return DEFAULT_PROJECT_STAGE;
+
+    }
+
+}
